ee52d04bf12dc51b54a28c772f79df3c1e0248ab
CoMe <vgrafodatskiy@mail.ru>
2023-03-09 10:46:51 +0700

    Примерная структура

--- src/App.js
+++ src/App.js
@@ -1,25 +1,166 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Image extends React.Component {
+
+    render(){
+        return(
+            <div>
+                <p>Image</p>
+            </div>
+        );
+    }
 }
 
-export default App;
+class Slider extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            min: props.min,
+            max: props.max,
+            value: props.value,
+            onChange: props.onChange,
+        };
+    }
+    render(){
+        return(
+            <div className="slider-container">
+                <input type="range" className="slider" min={this.state.min} max={this.state.max} value={this.state.value} onChange={this.state.onChange}/>
+            </div>
+        );
+    }
+}
+
+class SideMenu extends React.Component {
+    render(){
+        return(
+            <div>SideMenu</div>
+        );
+    }
+}
+
+class SideMenuItem extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            active: props.active,
+            onClick: props.onClick,
+        };
+    }
+
+    render(){
+        return(
+            <button className={this.state.active === true ? "side-menu-item-active" : "side-menu-item"} onclick={this.state.oncClick}>
+                {this.state.name}
+            </button>
+        )
+    }
+}
+
+const DEFAULT_OPTIONS = [
+  {
+    name: 'Brightness',
+    property: 'brightness',
+    value: 100,
+    range: {
+      min: 0,
+      max: 200
+    },
+    unit: '%'
+  },
+  {
+    name: 'Contrast',
+    property: 'contrast',
+    value: 100,
+    range: {
+      min: 0,
+      max: 200
+    },
+    unit: '%'
+  },
+  {
+    name: 'Saturation',
+    property: 'saturate',
+    value: 100,
+    range: {
+      min: 0,
+      max: 200
+    },
+    unit: '%'
+  },
+  {
+    name: 'Grayscale',
+    property: 'grayscale',
+    value: 0,
+    range: {
+      min: 0,
+      max: 100
+    },
+    unit: '%'
+  },
+  {
+    name: 'Sepia',
+    property: 'sepia',
+    value: 0,
+    range: {
+      min: 0,
+      max: 100
+    },
+    unit: '%'
+  },
+  {
+    name: 'Hue Rotate',
+    property: 'hue-rotate',
+    value: 0,
+    range: {
+      min: 0,
+      max: 360
+    },
+    unit: 'deg'
+  },
+  {
+    name: 'Blur',
+    property: 'blur',
+    value: 0,
+    range: {
+      min: 0,
+      max: 20
+    },
+    unit: 'px'
+  }
+]
+
+class ImageEditor extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            activeIndex: 0,
+            options: DEFAULT_OPTIONS,
+            activeOption: 0,
+        };
+    }
+    handleSliderChange(value){
+        console.log(value);
+    };
+
+    render() {
+        return (
+            <div className="image-editor">
+                <div className="image">
+                    <Image />
+                </div>
+                <div className="side-menu">
+                    <SideMenu />
+                </div>
+                <div className="slider">
+                    <Slider min="0" max="100" value="30" onChange={e => this.handleSliderChange(e.target.value)}></Slider>
+                </div>
+            </div>
+        );
+    }
+}
+
+export {ImageEditor};
