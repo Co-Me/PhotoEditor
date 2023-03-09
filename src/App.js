f19e668125c9a91b68ed0cd7ced867286f33d30f
CoMe <vgrafodatskiy@mail.ru>
2023-03-09 15:09:20 +0700

    Дизайн, рабочие кнопки и слайдер

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import React from "react";
 import "./App.css";
 
 class Image extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            image: props.image
+        };
+    }
 
     render(){
         return(
-            <div>
+            <div className="main-image">
                 <p>Image</p>
             </div>
         );
@@ -23,6 +30,14 @@ class Slider extends React.Component {
             onChange: props.onChange,
         };
     }
+
+    componentWillReceiveProps(nextProps) {
+        this.setState({ value: nextProps.value,
+                        min: nextProps.min,
+                        max: nextProps.max,
+                      });
+    }
+
     render(){
         return(
             <div className="slider-container">
@@ -42,20 +57,24 @@ class SideMenu extends React.Component {
             onClick: props.onClick
         };
     }
+
+    componentWillReceiveProps(nextProps) {
+        this.setState({ activeIndex: nextProps.activeIndex, options: nextProps.options});
+    }
+
     render(){
         return(
             this.state.options.map((option, index) => {
                 return (
                     <SideMenuItem
-                        name={option.name}
                         key = {index}
+                        name={option.name}
                         id = {index}
                         active={this.state.activeIndex === index}
-                        onClick={e => this.state.onClick(e)}
+                        onClick={this.state.onClick}
                     />
                 )
-            }
-            )
+            })
         );
     }
 }
@@ -65,17 +84,21 @@ class SideMenuItem extends React.Component {
         super(props);
 
         this.state = {
-            name: props.name,
             key: props.id,
+            name: props.name,
             id: props.id,
             active: props.active,
             onClick: props.onClick
         };
     }
 
+        componentWillReceiveProps(nextProps) {
+        this.setState({ active: nextProps.active});
+    }
+
     render(){
         return(
-            <button className={this.state.active === true ? "side-menu-item-active" : "side-menu-item"} onClick={e => this.state.onClick(e)}>
+            <button id={this.state.id} className={this.state.active === true ? "side-menu-item-active" : "side-menu-item"} onClick={this.state.onClick}>
                 {this.state.name}
             </button>
         )
@@ -87,70 +110,56 @@ const DEFAULT_OPTIONS = [
     name: 'Brightness',
     property: 'brightness',
     value: 100,
-    range: {
-      min: 0,
-      max: 200
-    },
+    min: 0,
+    max: 200,
     unit: '%'
   },
   {
     name: 'Contrast',
     property: 'contrast',
     value: 100,
-    range: {
-      min: 0,
-      max: 200
-    },
+    min: 0,
+    max: 200,
     unit: '%'
   },
   {
     name: 'Saturation',
     property: 'saturate',
     value: 100,
-    range: {
-      min: 0,
-      max: 200
-    },
+    min: 0,
+    max: 200,
     unit: '%'
   },
   {
     name: 'Grayscale',
     property: 'grayscale',
     value: 0,
-    range: {
       min: 0,
-      max: 100
-    },
+      max: 100,
     unit: '%'
   },
   {
     name: 'Sepia',
     property: 'sepia',
     value: 0,
-    range: {
-      min: 0,
-      max: 100
-    },
+    min: 0,
+    max: 100,
     unit: '%'
   },
   {
     name: 'Hue Rotate',
     property: 'hue-rotate',
     value: 0,
-    range: {
-      min: 0,
-      max: 360
-    },
+    min: 0,
+    max: 360,
     unit: 'deg'
   },
   {
     name: 'Blur',
     property: 'blur',
     value: 0,
-    range: {
-      min: 0,
-      max: 20
-    },
+    min: 0,
+    max: 20,
     unit: 'px'
   }
 ]
@@ -162,25 +171,22 @@ class ImageEditor extends React.Component {
         this.state = {
             activeIndex: 0,
             options: DEFAULT_OPTIONS,
+            image: null
         };
     }
 
     handleSideMenuItemClick(event){
         this.setState({activeIndex: event.target.id});
-        console.log(event.target);
     }
 
     handleSliderChange(event){
-        const nextOptions = this.state.options.map((c, i) => {
-            if (i === this.state.activeIndex)
-                c.value = event.target.value;
-            return c;
-        });
-        this.setState({
-            options: nextOptions
-        });
+        let nextOptions = []
+        for (let i = 0; i < this.state.options.length; i++) {
+            nextOptions.push(this.state.options[i]);
+        }
 
-        console.log(this.state.options[this.state.activeIndex].value);
+        nextOptions[this.state.activeIndex].value = event.target.value;
+        this.setState({ options: nextOptions });
     };
 
     render() {
@@ -190,13 +196,18 @@ class ImageEditor extends React.Component {
                     <Image />
                 </div>
                 <div className="side-menu">
-                    <SideMenu activeIndex={this.state.activeIndex} options={this.state.options} onClick={e => this.handleSideMenuItemClick(e)}></SideMenu>
+                    <SideMenu
+                        activeIndex={this.state.activeIndex}
+                        options={this.state.options}
+                        onClick={e => this.handleSideMenuItemClick(e)}>
+                    </SideMenu>
                 </div>
                 <div className="slider">
-                    <Slider min={this.state.options[this.state.activeIndex].range.min}
-                            max={this.state.options[this.state.activeIndex].range.max}
+                    <Slider id="s" min={this.state.options[this.state.activeIndex].min}
+                            max={this.state.options[this.state.activeIndex].max}
                             value={this.state.options[this.state.activeIndex].value}
-                            onChange={e => this.handleSliderChange(e)}></Slider>
+                            onChange={e => this.handleSliderChange(e)}>
+                    </Slider>
                 </div>
             </div>
         );
